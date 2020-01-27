5b44330aa03ef27cace7e015c3835f36e5dfcd07
qpzm <lhm1442@gmail.com>
2020-01-27 20:23:28 +0900

    11726-2*n tiling
    10007 is the smallest prime number >= 10000.
    fib(47) = 2971215073 > 2**31.

---
+++ 11726.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main() {
+    int N, a=1, b=1, tmp;
+    scanf("%d", &N);
+    for(int i=1; i<N; i++) {
+        tmp = b ;
+        b = (a + b)% 10007;
+        a = tmp;
+    }
+    printf("%d\n", b);
+}
