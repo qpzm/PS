2a2010cfea9559ebc2de835e629b6fd9f99e1468
qpzm <lhm1442@gmail.com>
2020-04-01 17:07:25 +0900

    BOJ_2493-Keep monotone sequence w/ stack

--- boj/2493.py
+++ boj/2493.py
@@ -1,13 +1,19 @@
 from sys import stdin, stdout
 
-MAX_HEIGHT = 100_000_000
+def find(stack, tower, loc):
+    while len(stack) != 0:
+        left_height, left_loc = stack[-1]
+        if  left_height < tower:
+            stack.pop()
+        else:
+            stack.append((tower, loc))
+            return left_loc
 
-def find(left_towers, tower, loc):
-    for i in range(loc - 1, -1, -1):
-        if left_towers[i] >= tower:
-            return i
+    stack.append((tower, loc))
+    return 0
 
 N = int(stdin.readline().rstrip())
-towers = [MAX_HEIGHT] + list(map(int, stdin.readline().rstrip().split()))
-for loc, tower in enumerate(towers[1:], start=1):
-    stdout.write(f'{find(towers, tower, loc)} ')
+stack = []
+towers = list(map(int, stdin.readline().rstrip().split()))
+for loc, tower in enumerate(towers, start=1):
+    stdout.write(f'{find(stack, tower, loc)} ')
