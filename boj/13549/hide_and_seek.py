d035eb13aa83e10ce55898d4bace6761a9bf8f73
qpzm <lhm1442@gmail.com>
2020-04-01 14:06:59 +0900

    BOJ_13549-Sol2: visit (v * 2 ** n) at once

--- boj/13549/hide_and_seek.py
+++ boj/13549/hide_and_seek.py
@@ -1,30 +1,27 @@
 from collections import deque
 from sys import stdin
 
+def visit_all(v, steps, queue, steps_to_v):
+    end = len(steps) - 1
+    while(0 <= v <= end and steps[v] == -1):
+        steps[v] = steps_to_v
+        queue.append(v)
+        v *= 2
+
 def solve(src, dest):
-    end = max(dest * 2 - 1, src)
+    end = max(dest + (dest - src) * 2 - 1, src)
     steps = [-1] * (end + 1)
     steps[src] = 0
     queue = deque([src])
+    visit_all(src * 2, steps, queue, steps[src])
 
     while queue:
         cur = queue.popleft()
         if cur == dest:
             return steps[cur]
 
-        if cur != 0:
-            v = cur * 2
-            while(0 <= v <= end and steps[v] == -1):
-                steps[v] = steps[cur]
-                queue.appendleft(v)
-                v *= 2
-
         for v in [cur - 1, cur + 1]:
-            while(0 <= v <= end and steps[v] == -1):
-                steps[v] = steps[cur] + 1
-                queue.append(v)
-                if v == 0: break
-                v *= 2
+            visit_all(v, steps, queue, steps[cur] + 1)
 
 def main():
     src, dest = map(int, stdin.readline().split())
