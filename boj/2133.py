541738fb3b51097bae6d25339d3b47f51b60b3b8
qpzm <lhm1442@gmail.com>
2020-04-13 12:35:36 +0900

    BOJ 2133. Tri Tiling

---
+++ boj/2133.py
@@ -0,0 +1,29 @@
+N = int(input())
+dp = [None] * 31
+for i in range(len(dp)):
+    if i == 0:
+        # 원래 폭이 0이면 둘 수 없으므로 0.
+        # 그런데 1 <= N <= 30 이므로 계산 편의를 위해 1로 둠.
+        dp[i] = 1
+    if i % 2:
+        dp[i] = 0
+    elif i == 2:
+        dp[i] = 3
+
+def solve(n):
+    if dp[n] is None:
+        result = solve(n-2) * solve(2)
+        i = n - 4
+        while(i >= 0):
+            result += dp[i] * 2
+            i -= 2
+        dp[n] = result
+
+    return dp[n]
+
+print(solve(N))
+assert(solve(1) == 0)
+assert(solve(2) == 3)
+assert(solve(3) == 0)
+assert(solve(4) == 11)
+assert(solve(6) == solve(4) * solve(2) + solve(2) * 2 + 2)
