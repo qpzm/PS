c6f6881fd9fb71462eda06c13755d859ea3cb904
qpzm <lhm1442@gmail.com>
2020-06-14 18:20:06 +0900

    Kakao Blind 2020 4. 가사 검색

--- programmers/kakao-blind-2020/search.py
+++ programmers/kakao-blind-2020/search.py
@@ -1,4 +1,6 @@
-def match(query, word):
+from functools import reduce
+
+def naive_match(query, word):
     if len(query) != len(word):
         return False
 
@@ -9,10 +11,74 @@ def match(query, word):
             return False
     return True
 
+def naive_solution(words, queries):
+    answer = list(
+        map(lambda q:
+            reduce(lambda x, y: x + y,
+                   map(lambda w:
+                       naive_match(q, w),
+                       words)
+                   ),
+            queries))
+    return answer
+
+# Passed solution
+# Problem: Testcase 5 requires 1.3 Gb because this saves child counts
+#          of each length in dict
+
+from collections import defaultdict
+
+class Node:
+    def __init__(self):
+        self.count = defaultdict(int) # 0: num of words ending here
+        self.children = [None] * 26
+
+    def put(self, word):
+        cur = self
+
+        while(len(word) != 0):
+            cur.count[len(word)] += 1
+            c = word[0]
+            child = cur.children[ord(c) - ord('a')]
+            if child == None:
+                cur.children[ord(c) - ord('a')] = Node()
+
+            cur = cur.children[ord(c) - ord('a')]
+            word = word[1:]
+
+        cur.count[len(word)] += 1
+
+    def search(self, word):
+        cur = self
+        i = 0
+        while(word[i] != '?'):
+            child = cur.children[ord(word[i]) - ord('a')]
+            if child == None:
+                return 0
+            cur = child
+            i += 1
+
+        return cur.count[len(word) - i]
+
+
 def solution(words, queries):
     answer = [0] * len(queries)
+    len_words = len(words)
+    prefix_tree = Node()
+    suffix_tree = Node()
+
+    for w in words:
+        prefix_tree.put(w)
+        suffix_tree.put(w[::-1])
+
     for i, q in enumerate(queries):
-        for w in words:
-            if match(q, w):
-                answer[i] += 1
+        if q[0] == '?' and q[-1] == '?':
+            ans = prefix_tree.count[len(q)]
+        elif q[0] == '?':
+            ans = suffix_tree.search(q[::-1])
+        else:
+            ans = prefix_tree.search(q)
+
+        answer[i] = ans
+
     return answer
--- programmers/kakao-blind-2020/search_test.py
+++ programmers/kakao-blind-2020/search_test.py
@@ -1,7 +1,27 @@
 import unittest
-from search import solution
+from search import solution, Node
 
 class SolutionTest(unittest.TestCase):
+    def test_put0(self):
+        tree = Node()
+        tree.put('abc')
+        self.assertEqual(1, tree.children[0].children[1].children[2].count[0])
+
+        tree.put('abd')
+        self.assertEqual(1, tree.children[0].children[1].children[3].count[0])
+
+    def test_search0(self):
+        tree = Node()
+        tree.put('abc')
+        self.assertEqual(1, tree.search('ab?'))
+
+    def test_search_question0(self):
+        tree = Node()
+        tree.put('abc')
+        tree.put('abd')
+        tree.put('abde')
+        self.assertEqual(2, tree.search('a??'))
+
     def test_right(self):
         words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
         queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
