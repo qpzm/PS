a424992e32977cf4ed9a8f252f921def31072adb
qpzm <lhm1442@gmail.com>
2020-08-30 01:05:48 +0900

    Leetcode 94. Inorder: recursive, less memory

--- leetcode/BinaryTreeInorderTraversal.java
+++ leetcode/BinaryTreeInorderTraversal.java
@@ -2,14 +2,31 @@ import java.util.List;
 import java.util.ArrayList;
 
 class BinaryTreeInorderTraversal {
+    // field
+    List<Integer> l = new ArrayList<Integer>();
+
     public List<Integer> inorderTraversal(TreeNode root) {
         if(root == null) {
-            return new ArrayList<Integer>();
+            return l;
         }
-        List<Integer> left = inorderTraversal(root.left);
-        List<Integer> right = inorderTraversal(root.right);
-        left.add(root.val);
-        left.addAll(right);
-        return left;
+
+        inorderTraversal(root.left);
+        l.add(root.val);
+        inorderTraversal(root.right);
+
+        return l;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
