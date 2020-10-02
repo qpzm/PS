6cca23523a7dcaac3c61c51219f339bc4a37db74
qpzm <lhm1442@gmail.com>
2020-10-03 07:56:54 +0900

    BOJ 18258. Queue 2 in kotlin
    Scanner를 사용했을 때는 시간 초과. BufferedReader, BufferedWriter를
    사용하니 통과

--- boj/kotlin/src/p18258/Queue.kt
+++ boj/kotlin/src/p18258/Queue.kt
@@ -1,4 +1,26 @@
 package p18258
 
-class queue {
+import java.util.*
+
+fun main() {
+    val l = ArrayDeque<Int>()
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    var n = br.readLine().toInt()
+    while(n-- != 0) {
+        val str = br.readLine()
+        val parsedList = str.split(' ')
+        when(parsedList[0]) {
+            "front" -> bw.write("${if(l.isEmpty()) -1 else l.peekFirst()}\n")
+            "back" -> bw.write("${if(l.isEmpty()) -1 else  l.peekLast()}\n")
+            "size" -> bw.write("${l.size}\n")
+            "empty" -> bw.write("${if(l.isEmpty()) 1 else 0}\n")
+            "pop" -> bw.write("${if(l.isEmpty()) -1 else l.removeFirst()}\n")
+            "push" -> l.add(parsedList[1].toInt())
+        }
+    }
+
+    br.close()
+    bw.close()
 }
