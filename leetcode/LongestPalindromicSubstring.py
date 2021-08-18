c9cfc784081971e0efc0b2be30753ec72d8ac01f
qpzm <lhm1442@gmail.com>
2021-08-18 16:59:25 +0900

    Leetcode 5. Longest Palindromic Substring O(N^2)

--- leetcode/LongestPalindromicSubstring.py
+++ leetcode/LongestPalindromicSubstring.py
@@ -1,35 +1,45 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) == 1:
-            return s
-        elif len(s) == 2:
-            if s[0] == s[1]:
-                return s
-            else:
-                return s[0]
-        else:
-            x = self.longestPalindrome(s[:-1])
-            if (s[0] == s[-1]):
-                y = s[0] + self.longestPalindrome(s[1:-1]) + s[-1]
-                if (len(x) > len(y)):
-                    return x
-                else:
-                    return y
-            else:
-                y = self.longestPalindrome(s[1:])
-                if (len(x) > len(y)):
-                    return x
-                else:
-                    return y
+        ret = s[0]  # len = 1일 때 아래가 range(1, 1) 이렇게 되므로
+        for i in range(2, 2 * len(s) - 1):
+            x = self._longestPalindrome(s, i)
+            if len(ret) < len(x):
+                ret = x
 
+        return ret
+
+    def _longestPalindrome(self, s: str, i: int) -> str:
+        x = ""
+        j = 0
+        if (i % 2 == 0): # 사이에서 시작하면 양쪽이 같아야 계속 확인
+            j = (i // 2) - 1
+            if (s[j] == s[j+1]):
+                x = s[j: j+2]
+                l, r = j-1, j+2
+            else:
+                return ""
+        else:
+            j = (i - 1) // 2
+            x = s[j]
+            l, r = j-1, j+1
+
+        while (0 <= l and r < len(s)):
+            if(s[l] == s[r]):
+                x = s[l] + x + s[r]
+                l = l - 1
+                r = r + 1
+            else:
+                break
+
+        return x
 
 
 def main():
-    problems = ["babad", "cbbd", "a", "ac", "aacabdkacaa"]
-    answers = ["bab", "bb", "a", "a", "aca"]
+    problems = ["babad", "cbbd", "a", "ac", "aacabdkacaa", "ccc"]
+    answers = ["bab", "bb", "a", "a", "aca", "ccc"]
     s = Solution()
     for p, a in zip(problems, answers):
-        print(s.longestPalindrome(p) == a)
+        print(s.longestPalindrome(p))
 
 
 if __name__ == '__main__':
