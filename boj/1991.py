7ae15a59b53e4f313834b8f0d587fd5683666b74
qpzm <lhm1442@gmail.com>
2022-09-24 16:25:58 +0900

    Add comments in boj/1991.py

--- boj/1991.py
+++ boj/1991.py
@@ -1,19 +1,22 @@
 from sys import stdin, stdout
 
-tree = dict()
+tree = dict() # {}
+
 N = int(stdin.readline().strip())
 for _ in range(N):
     node, left, right = stdin.readline().strip().split()
     tree[node] = (left, right)
 
+# left tree -> me -> right tree
 def inorder(node):
-    left, right = tree[node]
+    left, right = tree[node] # node = A left: B, right: C
     if left != '.':
         inorder(tree[node][0])
     stdout.write(node)
     if right != '.':
         inorder(tree[node][1])
 
+# me -> left tree -> right tree
 def preorder(node):
     left, right = tree[node]
     stdout.write(node)
